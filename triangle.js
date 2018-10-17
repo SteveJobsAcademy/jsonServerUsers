ff3c8e4bb7d86a5773ee5528e0d9d6f25229aa59
Carlo Leonardi <carloleonardi83@gmail.com>
2018-10-17 11:58:33 +0200

    add triangle.js

---
+++ triangle.js
@@ -0,0 +1,23 @@
+var isTriangle = function(a, b, c) {
+    return a + b + c > 2 * Math.max(a, b, c);
+}
+
+var getTriangleType = function(a, b, c) {
+    if (isTriangle(a, b, c)) {
+        if (a === b && b === c) {
+            return "EQUILATERO";
+        } else if (a == b || b == c || c == a) {
+            return "ISOSCELE";
+        } else {
+            return "SCALENO";
+        }
+    } else {
+        return "NOT TRIANGLE";
+    }
+}
+
+
+console.log(getTriangleType(11, 1, 1));
+console.log(getTriangleType(11, 9, 3));
+console.log(getTriangleType(4, 2, 4));
+console.log(getTriangleType(8, 8, 8));
