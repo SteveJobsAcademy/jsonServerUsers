bf3d7659acf5f88f09ad4ec3ffba106e962b0c2b
Carlo Leonardi <carloleonardi83@gmail.com>
2018-10-26 13:02:30 +0200

    add function to convert caeser cript

--- cript.js
+++ cript.js
@@ -22,7 +22,27 @@ function decript(sentence) {
     }
     return response;
 }
-var x = 'ciao sono carlo!';
-var criptSentence = cript(x);
-console.log(criptSentence);
-console.log(decript(criptSentence));
+//var x = 'ciao sono carlo!';
+//var criptSentence = cript(x);
+//console.log(criptSentence);
+//console.log(decript(criptSentence));
+
+
+function caesar(sentence, shift){
+    var response = '';
+    for(var letter of sentence) {
+        var index = alphabeta.indexOf(letter);
+        var finalIndex = index + shift;
+        finalIndex = finalIndex % alphabeta.length;
+        if (finalIndex < 0) {
+            finalIndex = alphabeta.length + finalIndex;
+        }
+        var newLetter = alphabeta.charAt(finalIndex);
+        response += newLetter;
+    }
+    return response;
+}
+var x = caesar('caio', 50);
+console.log("x:", x);
+var y = caesar(x, -50);
+console.log("y:", y);
